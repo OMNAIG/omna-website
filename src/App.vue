8e98868922c6246cd289333826d1a0c999bb5e91
Gabriel de Souza <gabrieldaniel@gec.inatel.br>
2025-02-12 23:49:05 -0300

    initial project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view />
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+  color: var(--color-secondary);
+  font-family: "Onest", Avenir, Helvetica, Arial, sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 nav {
@@ -26,5 +33,11 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+:root {
+  --color-bg: #001C1A;
+  --color-primary: #CEFF00;
+  --color-secondary: #FFF;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Omna logo" src="../assets/omna.svg" />
+    <p>Loading...</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style>
+.home {
+  background-color: #001C1A;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+}
+.home::before {
+  content: "";
+  background-image: url("../assets/noise.gif");
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0.04;
+}
+.home p {
+  color: var(--color-primary);
+  font-size: 2.5rem;
+  font-weight: 500;
+  animation: pulse 3s infinite;
+}
+@keyframes pulse {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+}
+</style>
 
+<script>
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
